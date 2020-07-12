379e57bda23004721a64d4e2d5c3e845f0d3c993
Sahar Kausar <saharkausar@gmail.com>
2020-07-12 00:05:29 -0700

    Adding removeVowel.py
    Completed Week 8, Section 12.2 challenge problem

---
+++ Week 8 Coding Challenges/List Comprehension/removeVowel.py
@@ -0,0 +1,18 @@
+#   SCRIPT: removeVowel.py
+#   AUTHOR: Sahar Kausar
+#   CONTACT: saharkausar@gmail.com
+#
+#   Anita Borg - Python Certification Course
+#
+#   DESCRIPTION: Take a string as an input and return a string with vowels removed.
+
+#Accepts an inputted string from the user and stores them in a list
+enterInput = [char for char in input("Please enter a sentence: ")]
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+#Removes the vowels from the inputted list
+new_list = ''.join([x for x in enterInput if x not in vowels])
+
+#Prints out the new list
+print(new_list)
