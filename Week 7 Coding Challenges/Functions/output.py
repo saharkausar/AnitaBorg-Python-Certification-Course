b1a92a2df8603841374cb05a4d5fe3abd79c5b25
Sahar Kausar <saharkausar@gmail.com>
2020-07-11 20:59:55 -0700

    Adding output.py
    Completed Week 7, Section 10.2 challenge problem

---
+++ Week 7 Coding Challenges/Functions/output.py
@@ -0,0 +1,24 @@
+#   SCRIPT: output.py
+#   AUTHOR: Sahar Kausar
+#   CONTACT: saharkausar@gmail.com
+#
+#   Anita Borg - Python Certification Course
+#
+#   DESCRIPTION: What is the output of the following code?
+
+"""
+Code Input:
+
+i = 1
+
+while i < 5:
+    if i == 6:
+        break
+    print(i, end = " ")
+    i = i + 1
+
+else:
+    print("Else is also printed")
+"""
+
+print("The output of the above code is:\n1 2 3 4 Else is also printed")
