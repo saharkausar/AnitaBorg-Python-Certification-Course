70b468e75f95eead3e364484a0162c06b26d8f55
Sahar Kausar <saharkausar@gmail.com>
2020-07-20 17:04:37 -0700

    Updating matrix.py
    Updated matrix.py to improve the user input (takes in two inputs per matrix by the user instead of four inputs per matrix row by the user) and cleaned up code to pass in only needed arguments.

--- Week 9 Coding Challenges/Inheritance/matrix.py
+++ Week 9 Coding Challenges/Inheritance/matrix.py
@@ -52,19 +52,25 @@ class two_D(one_D):
     #Method that performs matrix multiplication on two given 2 dimensional matrices by calling the dot_prod() function of the super class
     def dot_prod(self, array3, array4):
 
-        matrix = [[super().dot_prod(array1, array2), super().dot_prod(array1, array3)], [super().dot_prod(array4, array2), (super().dot_prod(array4, array3))]]
+        print("The first matrix (Matrix A) is:", [[matrixA[0],matrixA[1]],[matrixA[2],matrixA[3]]])
+        print("The second matrix (Matrix B) is:", [[matrixB[0],matrixB[1]],[matrixB[2],matrixB[3]]])
 
-        return matrix
+        #Splits the matrices from the user input
+        firstInput = [matrixA[0], matrixA[1]] #[[a1 a2]]
+        secondInput =  [matrixB[0], matrixB[2]] #[[b1 b3]]
+        thirdInput = [matrixB[1], matrixB[3]] #[[b2 b4]]
+        fourthInput = [matrixA[2], matrixA[3]] #[[a3 a4]]
 
-#Array based on a1, a2 from the example
-array1 = [int(m) for m in input("Please enter [a1 a2] for the first matrix 'A' separated by a space (Sample Input Example: 1 2): ").split()]
-#Array based on b1, b3 from the example
-array2 = [int(m) for m in input("Please enter [b1 b3] for the second matrix 'B' separated by a space (Sample Input Example: 5 7): ").split()]
-#Array based on b2, b4 from the example
-array3 = [int(m) for m in input("Please enter [b2 b4] for the second matrix 'B' separated by a space (Sample Input Example: 6 8): ").split()]
-#Array based on a3, a4 from the example
-array4 = [int(m) for m in input("Please enter [a3 a4] for the first matrix 'A' separated by a space (Sample Input Example: 3 4): ").split()]
+        #Calculates the dot product from the split matrices by feeding it back into one_D.dot_prod()
+        matrixCalc = [[super().dot_prod(firstInput, secondInput), super().dot_prod(firstInput, thirdInput)], [super().dot_prod(fourthInput, secondInput), (super().dot_prod(fourthInput, thirdInput))]]
+
+        return matrixCalc
+
+#Input for the first matrix [a1 a2 a3 a4]
+matrixA = [int(m) for m in input("Please enter [a1 a2 a3 a4] for the first matrix 'A' separated by a space (Sample Input Example: 1 2 3 4): ").split()]
+#Input for the second matrix [b1 b2 b3 b4]
+matrixB = [int(m) for m in input("Please enter [b1 b2 b3 b4] for the second matrix 'B' separated by a space (Sample Input Example: 5 6 7 8): ").split()]
 
 #Create an object and print the result
-finalMatrix = two_D(array3, array4)
-print(finalMatrix.dot_prod(array3, array4))
+finalMatrix = two_D(matrixA, matrixB)
+print("The dot product of Matrix A and B is:", finalMatrix.dot_prod(matrixA, matrixB))
